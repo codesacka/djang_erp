d151441af2fa0318d3226959a397e38d9c438719
klunwebale@gmail.com <klunwebale@gmail.com>
2019-10-18 07:18:30 +0300

    ##bankAccounts ,Deposit,Payment Update of the model structure

--- modules/accounting/accountClass/models.py
+++ modules/accounting/accountClass/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
+class AccountClass (models.Model):
+  name = models.CharField(max_length=300, default=0,null=True,blank=True)
+  user     =models.ForeignKey(User,on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True,auto_now=False)
+  updated_at = models.DateTimeField(auto_now_add=False,auto_now=True)
+
+  class Meta:
+        db_table = "account_class"
+
+        def __str__ (self):
+            return  self.name
--- modules/accounting/glAccounts/models.py
+++ modules/accounting/glAccounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
+from modules.accounting.accountClass.models import AccountClass
 # Create your models here.
+class AccountGl (models.Model):
+  name = models.CharField(max_length=300, default=0,null=True,blank=True)
+  accountclass = models.ForeignKey(AccountClass,on_delete=models.CASCADE)
+  manualentry = models.CharField(max_length=1, default=0,null=True,blank=True)
+  glcode= models.CharField(max_length=300, default=0,null=True,blank=True)
+  parent =models.IntegerField(default=0)
+  description = models.TextField(null=True,blank=True)
+  user         =models.ForeignKey(User,on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True,auto_now=False)
+  updated_at = models.DateTimeField(auto_now_add=False,auto_now=True)
+
+  class Meta:
+        db_table = "gl_account"
+
+        def __str__ (self):
+            return  self.name
